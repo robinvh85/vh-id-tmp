d1807bad39e4889c697e9306a06ed5983084ed8b
VH <vh@Apples-MacBook-Pro.local>
2019-06-04 05:34:27 +0700

    Add provider google and facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable,
+         :recoverable, :rememberable, :trackable, #:validatable,
          :omniauthable
-        #  :omniauth_providers => [:github]
 
   def self.from_omniauth(auth)  
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
